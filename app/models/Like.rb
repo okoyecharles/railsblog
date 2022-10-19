eafcaeb819401d9daf4fb7648ff28facc48ba2a2
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-19 11:57:03 +0100

    Create and test required methods

--- app/models/Like.rb
+++ app/models/Like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_create :update_like_counter
+
+  def update_like_counter
+    post.update(likes_counter: post.likes.size)
+  end
 end
--- app/models/Post.rb
+++ app/models/Post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  after_create :update_post_counter
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.size)
+  end
+
+  def recent_comments
+    comments.last(5)
+  end
 end
