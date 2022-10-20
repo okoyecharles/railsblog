099bdd87ff8c276faa29696c9162e5d3f6fb48b2
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-20 10:37:59 +0100

    fix errors

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -3,10 +3,9 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  validates :title, presence: true
-  validates :title, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_create :update_post_counter
 
