b0468dfcac276924482cfe0444ef57164c6fbd5b
OkoyeCharles <okoyecharles509@gmail.com>
2022-10-28 08:11:18 +0100

    Render view for comments/new & add like functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts[params[:id].to_i - 1]
-    @post_idx = @user.posts.find_index(@post) + 1
+    @post_idx = params[:id]
   end
 
   def new
