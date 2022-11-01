aa07ce233afd19b7eaf72389dc1ee277edadefe4
OkoyeCharles <okoyecharles509@gmail.com>
2022-11-01 14:43:25 +0100

    Add migration (AddDeviseToUsers)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
