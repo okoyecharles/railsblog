cb995fb5b7da5056ba61be47105f79cc2b271c76
OkoyeCharles <okoyecharles509@gmail.com>
2022-11-01 16:56:21 +0100

    Add reviewer(1) change suggestions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,21 +41,6 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: 'example@email.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'example.com',
-    user_name: Rails.application.credentials.gmail[:email],
-    password: Rails.application.credentials.gmail[:password],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
