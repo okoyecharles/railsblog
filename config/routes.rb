c3f379a036276d1ba83a8012d070b3a2db17b5a0
kadenyi <treykadenyi@gmail.com>
2022-11-01 17:08:19 +0300

    Create signup and sign_in views and add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do 
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
