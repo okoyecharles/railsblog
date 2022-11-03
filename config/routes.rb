6bec312d6c3e76aaaf919fec786d726c496e5501
OkoyeCharles <okoyecharles509@gmail.com>
2022-11-03 14:44:38 +0100

    Create API endpoint to list all posts for a user.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
       resources :likes, only: %i[create]
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index show] do
+        resources :posts, only: %i[index create]
+      end
+    end
+  end
 end
