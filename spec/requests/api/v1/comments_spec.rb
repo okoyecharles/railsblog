bc99692fd06d3442ae407ecd816d286c1e6eb849
kadenyi <treykadenyi@gmail.com>
2022-11-04 17:23:49 +0300

    Write API integration specs for comments controller

---
+++ spec/requests/api/v1/comments_spec.rb
@@ -0,0 +1,128 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/comments', type: :request do
+  before(:each) do
+    @user = User.create(name: 'Example User', photo: 'example.jpg', bio: 'Teacher from Nigeria.')
+    @post = Post.create(author_id: @user.id, title: 'Hello', text: 'This is my test post')
+  end
+
+    
+  path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
+    # You'll want to customize the parameter types...
+    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
+    parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
+
+  
+    get('list comments') do
+      response(200, 'successful') do
+        let(:user_id) { @user.id }
+        let(:post_id) { post.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create comment') do
+      response(200, 'successful') do
+        let(:user_id) { @user.id }
+        let(:post_id) { post.id }
+        consumes 'application/json'
+        parameter name: :comment, in: :body, schema: {
+          type: :object,
+          properties: {
+            author_id: { type: :integer },
+            post_id: { type: :integer },
+            text: { type: :string }
+          },
+          required: %w[author_id post_id text]
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/{user_id}/posts/{post_id}/comments/new' do
+    # You'll want to customize the parameter types...
+    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
+    parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
+
+    get('new comment') do
+      response(200, 'successful') do
+        let(:user_id) { @user.id }
+        let(:post_id) { @post.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/{user_id}/posts/{post_id}/comments/{id}/edit' do
+    # You'll want to customize the parameter types...
+    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
+    parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('edit comment') do
+      response(200, 'successful') do
+        let(:user_id) { @user.id }
+        let(:post_id) { @post.id }
+        let(:id) { id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/{user_id}/posts/{post_id}/comments/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
+    parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show comment') do
+      response(200, 'successful') do
+        let(:user_id) { @user.id }
+        let(:post_id) { @post.id }
+        let(:id) { id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
